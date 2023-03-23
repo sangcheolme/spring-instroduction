c21929c66aad5d900afe9c159a525b89bbc76204
SangCheol <scpark0698@gmail.com>
2023-03-24 01:08:58 +0900

    5. 회원 관리 예제 - 웹 MVC 개발

--- src/main/java/hello/springintroduction/repository/MemoryMemberRepository.java
+++ src/main/java/hello/springintroduction/repository/MemoryMemberRepository.java
@@ -1,6 +1,7 @@
 package hello.springintroduction.repository;
 
 import hello.springintroduction.domain.Member;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
 import java.util.*;
